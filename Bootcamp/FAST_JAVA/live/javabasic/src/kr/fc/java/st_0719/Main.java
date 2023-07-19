74bf0094775d264c5b295a7bf8bece39890908c0
cyPark95 <parkcy1007@gmail.com>
2023-07-19 13:07:10 +0900

    0719 Java 실시간 강의

---
+++ Bootcamp/FAST_JAVA/live/javabasic/src/kr/fc/java/st_0719/Main.java
@@ -0,0 +1,8 @@
+package kr.fc.java.st_0719;
+
+public class Main {  // 메인, 시작 클래스
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");  // 스트림(Stream) = 데이터의 통로(I/O)
+    }
+}
---
+++ Bootcamp/FAST_JAVA/live/javabasic/src/kr/fc/model/Book.java
@@ -0,0 +1,34 @@
+package kr.fc.model;
+
+public class Book {
+    private String title;
+    private int price;
+
+    // 생략된 생성자: 기본(default) 생성자
+    // 생성자의 이름은 클래스 이름과 동일해야 한다.
+    // 반환(return) 타입이 없다.
+    public Book() {
+        // 자신의 모든 멤버들을 메모리에 로딩한다.
+    }
+
+    public Book(String title, int price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
