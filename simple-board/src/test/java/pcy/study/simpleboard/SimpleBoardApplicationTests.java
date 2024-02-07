4243ad7b8c5f80a34e18342bda726200fa0d8411
cyPark95 <parkcy1007@gmail.com>
2024-02-07 11:53:20 +0900

    Create Simple Board Project

---
+++ simple-board/src/test/java/pcy/study/simpleboard/SimpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package pcy.study.simpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
