1ceed8519bdade1e3163225ad0e27cd37bc773c0
cyPark95 <parkcy1007@gmail.com>
2024-02-19 16:06:10 +0900

    [Simple-Board] 댓글 삭제

--- simple-board/src/main/java/pcy/study/simpleboard/reply/controller/ReplyController.java
+++ simple-board/src/main/java/pcy/study/simpleboard/reply/controller/ReplyController.java
@@ -4,8 +4,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pcy.study.simpleboard.common.interceptor.Authenticated;
 import pcy.study.simpleboard.reply.model.ReplyCreateRequest;
+import pcy.study.simpleboard.reply.model.ReplyDeleteRequest;
 import pcy.study.simpleboard.reply.service.ReplyService;
 
 @RestController
@@ -19,5 +24,11 @@ public class ReplyController {
     public ResponseEntity<Long> create(@RequestBody @Valid ReplyCreateRequest replyCreateRequest) {
         var saveResult = replyService.save(replyCreateRequest);
         return new ResponseEntity<>(saveResult, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<Void> delete(@RequestBody @Valid ReplyDeleteRequest replyDeleteRequest) {
+        replyService.delete(replyDeleteRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- simple-board/src/main/java/pcy/study/simpleboard/reply/service/ReplyService.java
+++ simple-board/src/main/java/pcy/study/simpleboard/reply/service/ReplyService.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pcy.study.simpleboard.post.service.PostService;
+import pcy.study.simpleboard.reply.db.Reply;
 import pcy.study.simpleboard.reply.db.ReplyRepository;
 import pcy.study.simpleboard.reply.model.ReplyCreateRequest;
+import pcy.study.simpleboard.reply.model.ReplyDeleteRequest;
 
 @Slf4j
 @Service
@@ -26,5 +28,25 @@ public class ReplyService {
         entity.registerPost(post);
         replyRepository.save(entity);
         return entity.getId();
+    }
+
+    @Transactional
+    public void delete(ReplyDeleteRequest replyDeleteRequest) {
+        var reply = findReplyById(replyDeleteRequest.id());
+        log.info("Delete Reply = {}", reply);
+
+        matchesPassword(reply, replyDeleteRequest.password());
+        replyRepository.delete(reply);
+    }
+
+    private Reply findReplyById(Long id) {
+        return replyRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException(String.format("[%d] 존재하지 않는 댓글입니다.", id)));
+    }
+
+    private void matchesPassword(Reply reply, String password) {
+        if (!reply.getPassword().equals(password)) {
+            throw new IllegalArgumentException("잘못된 패스워드 입니다.");
+        }
     }
 }
