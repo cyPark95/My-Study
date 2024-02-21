6c2f9a8a3d5bbf3d9d495eda346e21727a450af3
cyPark95 <parkcy1007@gmail.com>
2024-02-21 17:08:00 +0900

    [Delivery] 인증 Interceptor 설정

---
+++ Signiture-Backend/delivery/api/src/main/java/pcy/study/api/config/web/WebConfig.java
@@ -0,0 +1,40 @@
+package pcy.study.api.config.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import pcy.study.api.interceptor.AuthorizationInterceptor;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final List<String> OPEN_API = List.of(
+            "/open-api/**"
+    );
+
+    private static final List<String> DEFAULT_EXCLUDE = List.of(
+            "/",
+            "favicon.ico",
+            "/error"
+    );
+
+    private static final List<String> SWAGGER = List.of(
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    );
+
+    private final AuthorizationInterceptor authorizationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor)
+                .excludePathPatterns(OPEN_API)
+                .excludePathPatterns(DEFAULT_EXCLUDE)
+                .excludePathPatterns(SWAGGER);
+    }
+}
