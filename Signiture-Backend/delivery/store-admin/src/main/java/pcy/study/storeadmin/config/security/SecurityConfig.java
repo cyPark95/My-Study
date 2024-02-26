56ebd95fb7351c2cae43779e2595c948a5c1bbac
cyPark95 <parkcy1007@gmail.com>
2024-02-26 11:22:02 +0900

    [Delivery] Spring Security 설정

---
+++ Signiture-Backend/delivery/store-admin/src/main/java/pcy/study/storeadmin/config/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package pcy.study.storeadmin.config.security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] SWAGGER_URLS = new String[]{
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    };
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers(SWAGGER_URLS).permitAll()
+                        .requestMatchers("/open-api/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
