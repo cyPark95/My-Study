340727d92ea6d72fd2508e7d901e6175c5938a7c
cyPark95 <parkcy1007@gmail.com>
2024-03-08 12:13:32 +0900

    [Delivery] 가게 메뉴 관련 테스트

---
+++ Signiture-Backend/delivery/store-admin/src/test/java/pcy/study/storeadmin/domain/storemenu/service/StoreMenuServiceTest.java
@@ -0,0 +1,51 @@
+package pcy.study.storeadmin.domain.storemenu.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import pcy.study.db.storemenu.StoreMenu;
+import pcy.study.db.storemenu.StoreMenuRepository;
+import pcy.study.storeadmin.config.annotation.ServiceTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static pcy.study.storeadmin.utility.StoreMenuUtils.STORE_MENU_ID;
+import static pcy.study.storeadmin.utility.StoreMenuUtils.createStoreMenu;
+
+@ServiceTest
+class StoreMenuServiceTest {
+
+    private StoreMenuService storeMenuService;
+
+    @Autowired
+    private StoreMenuRepository storeMenuRepository;
+
+    @BeforeEach
+    void setUp() {
+        storeMenuService = new StoreMenuService(storeMenuRepository);
+    }
+
+    @Test
+    @DisplayName("가게 메뉴 조회")
+    void getStoreMenuWithThrow() {
+        // given
+        StoreMenu storeMenu = createStoreMenu();
+        storeMenuRepository.save(storeMenu);
+
+        // when
+        StoreMenu result = storeMenuService.getStoreMenuWithThrow(STORE_MENU_ID);
+
+        // then
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    @DisplayName("가게 메뉴 조회 - 없는 ID")
+    void getStoreMenuWithThrow_failureNotFound() {
+        // when
+        // then
+        assertThatThrownBy(() -> storeMenuService.getStoreMenuWithThrow(STORE_MENU_ID))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
---
+++ Signiture-Backend/delivery/store-admin/src/test/java/pcy/study/storeadmin/utility/StoreMenuUtils.java
@@ -0,0 +1,27 @@
+package pcy.study.storeadmin.utility;
+
+import pcy.study.db.storemenu.StoreMenu;
+
+import java.math.BigDecimal;
+
+import static pcy.study.storeadmin.utility.StoreUtils.STORE_ID;
+
+public class StoreMenuUtils {
+
+    public static final Long STORE_MENU_ID = 1L;
+    public static final String STORE_MENU_NAME = "store menu name";
+    public static final BigDecimal STORE_MENU_AMOUNT = BigDecimal.valueOf(8000.0);
+    public static final String STORE_MENU_THUMBNAIL = "https://www.thumbnail.xxx";
+
+    private StoreMenuUtils() {
+    }
+
+    public static StoreMenu createStoreMenu() {
+        return StoreMenu.builder()
+                .storeId(STORE_ID)
+                .name(STORE_MENU_NAME)
+                .amount(STORE_MENU_AMOUNT)
+                .thumbnailUrl(STORE_MENU_THUMBNAIL)
+                .build();
+    }
+}
