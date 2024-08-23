7e96ecfefbca67e6b7d51a3212f8881eb2a89708
cyPark95 <parkcy1007@gmail.com>
2024-08-23 14:52:18 +0900

    Kotlin 표준함수 Let

---
+++ Signiture-Backend/kotlin/study/src/main/kotlin/function/standard/Data.kt
@@ -0,0 +1,13 @@
+package pcy.study.function.standard
+
+data class UserDto(
+    var name: String? = null,
+)
+
+data class UserEntity(
+    var name: String? = null,
+)
+
+data class UserResponse(
+    var userName: String? = null
+)
