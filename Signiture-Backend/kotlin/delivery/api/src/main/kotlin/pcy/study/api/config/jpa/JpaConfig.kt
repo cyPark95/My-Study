ab5bf1cd1d76a9e824b2e2402ba648daec6025cc
cyPark95 <parkcy1007@gmail.com>
2024-08-25 15:58:05 +0900

    [Delivery] Jpa 설정 Kotlin으로 변경

--- Signiture-Backend/kotlin/delivery/api/src/main/kotlin/pcy/study/api/config/jpa/JpaConfig.kt
+++ Signiture-Backend/kotlin/delivery/api/src/main/kotlin/pcy/study/api/config/jpa/JpaConfig.kt
@@ -1,11 +1,10 @@
-package pcy.study.api.config.jpa;
+package pcy.study.api.config.jpa
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EntityScan(basePackages = "pcy.study.db")
-@EnableJpaRepositories(basePackages = "pcy.study.db")
-public class JpaConfig {
-}
+@EntityScan(basePackages = ["pcy.study.db"])
+@EnableJpaRepositories(basePackages = ["pcy.study.db"])
+class JpaConfig
