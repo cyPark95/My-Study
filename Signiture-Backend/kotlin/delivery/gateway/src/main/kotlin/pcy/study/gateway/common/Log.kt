92920df6571fa61aaca2ca49bf6086242c5a3278
cyPark95 <parkcy1007@gmail.com>
2024-12-30 10:36:23 +0900

    Gateway Route YML 방식 설정

---
+++ Signiture-Backend/kotlin/delivery/gateway/src/main/kotlin/pcy/study/gateway/common/Log.kt
@@ -0,0 +1,9 @@
+package pcy.study.gateway.common
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+interface Log {
+
+    val log: Logger get() = LoggerFactory.getLogger(this.javaClass)
+}
