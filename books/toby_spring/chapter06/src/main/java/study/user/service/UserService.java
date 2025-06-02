ad8014f1e435bc088c7b3ddf0e566c5242fb1966
cyPark95 <parkcy1007@gmail.com>
2025-06-02 10:35:14 +0900

    토비의 스프링 6장 정리

---
+++ books/toby_spring/chapter06/src/main/java/study/user/service/UserService.java
@@ -0,0 +1,88 @@
+package study.user.service;
+
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import study.user.dao.UserDao;
+import study.user.domain.Level;
+import study.user.domain.User;
+
+import java.util.List;
+
+public class UserService {
+
+    private UserDao userDao;
+    private PlatformTransactionManager transactionManager;
+    private MailSender mailSender;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public void setMailSender(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void upgradeLevels() {
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            upgradeLevelsInternal();
+            transactionManager.commit(status);
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw e;
+        }
+    }
+
+    public void add(User user) {
+        if (user.getLevel() == null) {
+            user.setLevel(Level.BASIC);
+        }
+
+        userDao.add(user);
+    }
+
+    protected void upgradeLevel(User user) {
+        user.upgradeLevel();
+        userDao.update(user);
+        sendUpgradeEmail(user);
+    }
+
+    private void upgradeLevelsInternal() {
+        List<User> users = userDao.getAll();
+
+        for (User user : users) {
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
+            }
+        }
+    }
+
+    private boolean canUpgradeLevel(User user) {
+        Level currentLevel = user.getLevel();
+
+        return switch (currentLevel) {
+            case Level.BASIC -> user.getLogin() >= User.MIN_LOGCOUNT_FOR_SILVER;
+            case Level.SILVER -> user.getRecommend() >= User.MIN_RECCOMEND_FOR_GOLD;
+            case Level.GOLD -> false;
+            default -> throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+        };
+    }
+
+    private void sendUpgradeEmail(User user) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setFrom("useradmin@ksug.org");
+        mailMessage.setSubject("Upgrade 안내");
+        mailMessage.setText(String.format("사용자님의 등급이 %s로 업그레이드되었습니다", user.getLevel().name()));
+
+        mailSender.send(mailMessage);
+    }
+}
---
+++ books/toby_spring/chapter06/src/test/java/study/user/service/UserServiceTest.java
@@ -0,0 +1,139 @@
+package study.user.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.PlatformTransactionManager;
+import study.user.dao.UserDao;
+import study.user.domain.Level;
+import study.user.domain.User;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:test-application-context.xml")
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    @Autowired
+    private MailSender mailSender;
+
+    private List<User> users;
+
+    @BeforeEach
+    void setUp() {
+        this.users = List.of(
+                new User("bumjin", "박범진", "p1", Level.BASIC, User.MIN_LOGCOUNT_FOR_SILVER - 1, 0, ""),
+                new User("joytouch", "강명성", "p2", Level.BASIC, User.MIN_LOGCOUNT_FOR_SILVER, 0, ""),
+                new User("erwins", "신승한", "p3", Level.SILVER, 60, User.MIN_RECCOMEND_FOR_GOLD - 1, ""),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, User.MIN_RECCOMEND_FOR_GOLD, ""),
+                new User("green", "오민규", "p5", Level.GOLD, 100, Integer.MAX_VALUE, "")
+        );
+    }
+
+    @Test
+    void upgradeLevels() throws Exception {
+        userDao.deleteAll();
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        MockMailSender mockMailSender = new MockMailSender();
+        userService.setMailSender(mockMailSender);
+
+        userService.upgradeLevels();
+
+        checkLevelUpgraded(false, users.get(0));
+        checkLevelUpgraded(true, users.get(1));
+        checkLevelUpgraded(false, users.get(2));
+        checkLevelUpgraded(true, users.get(3));
+        checkLevelUpgraded(false, users.get(4));
+
+        List<String> requests = mockMailSender.getRequests();
+        assertEquals(2, requests.size());
+        assertEquals(users.get(1).getEmail(), requests.get(0));
+        assertEquals(users.get(3).getEmail(), requests.get(1));
+    }
+
+    @Test
+    void add() {
+        userDao.deleteAll();
+
+        User userWithLevel = users.get(4);
+        User userWithoutLevel = users.get(0);
+        userWithoutLevel.setLevel(null);
+
+        userService.add(userWithLevel);
+        userService.add(userWithoutLevel);
+
+        User userWithLevelRead = userDao.get(userWithLevel.getId());
+        User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+
+        assertEquals(userWithLevel.getLevel(), userWithLevelRead.getLevel());
+        assertEquals(Level.BASIC, userWithoutLevelRead.getLevel());
+    }
+
+    @Test
+    void upgradeAllOrNothing() throws Exception {
+        UserService testUserService = new TestUserService(users.get(3).getId());
+        testUserService.setUserDao(userDao);
+        testUserService.setTransactionManager(transactionManager);
+        testUserService.setMailSender(mailSender);
+
+        userDao.deleteAll();
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        try {
+            testUserService.upgradeLevels();
+        } catch (TestUserServiceException ignored) {
+        }
+
+        checkLevelUpgraded(false, users.get(1));
+    }
+
+    private void checkLevelUpgraded(boolean expectedUpgraded, User user) {
+        User userUpdate = userDao.get(user.getId());
+        if (expectedUpgraded) {
+            assertEquals(user.getLevel().nextLevel(), userUpdate.getLevel());
+        } else {
+            assertEquals(user.getLevel(), userUpdate.getLevel());
+        }
+    }
+
+    static class TestUserService extends UserService {
+
+        private final String id;
+
+        public TestUserService(String id) {
+            this.id = id;
+        }
+
+        @Override
+        public void upgradeLevel(User user) {
+            if (user.getId().equals(this.id)) {
+                throw new TestUserServiceException();
+            }
+
+            super.upgradeLevel(user);
+        }
+    }
+
+    static class TestUserServiceException extends RuntimeException {
+    }
+}
