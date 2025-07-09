5376decba470d6a181769e20dcf9439104e07a73
cyPark95 <parkcy1007@gmail.com>
2025-07-09 21:42:33 +0900

    Spring Cloud Gateway

---
+++ spring-cloud-gateway/api-gateway/src/main/java/pcy/study/apigateway/config/RouteConfig.java
@@ -0,0 +1,37 @@
+package pcy.study.apigateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pcy.study.apigateway.filter.SecondApiPrivateFilter;
+import pcy.study.apigateway.filter.SecondApiPublicFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class RouteConfig {
+
+    private final SecondApiPublicFilter secondApiPublicFilter;
+    private final SecondApiPrivateFilter secondApiPrivateFilter;
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(predicateSpec -> predicateSpec.path("/gateway/second/public/**")
+                        .filters(gatewayFilterSpec -> {
+                            gatewayFilterSpec.rewritePath("/gateway(?<segment>.*)", "${segment}");
+                            gatewayFilterSpec.filter(secondApiPublicFilter.apply(new SecondApiPublicFilter.Config()));
+                            return gatewayFilterSpec;
+                        })
+                        .uri("http://localhost:8090"))
+                .route(predicateSpec -> predicateSpec.path("/gateway/second/private/**")
+                        .filters(gatewayFilterSpec -> {
+                            gatewayFilterSpec.rewritePath("/gateway(?<segment>.*)", "${segment}");
+                            gatewayFilterSpec.filter(secondApiPrivateFilter.apply(new SecondApiPrivateFilter.Config()));
+                            return gatewayFilterSpec;
+                        })
+                        .uri("http://localhost:8090"))
+                .build();
+    }
+}
---
+++ spring-cloud-gateway/first-api/src/main/java/pcy/study/firstapi/controller/CheckController.java
@@ -0,0 +1,20 @@
+package pcy.study.firstapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first")
+public class CheckController {
+
+    @GetMapping("/public/check")
+    public String publicCheck() {
+        return "Success Public First API";
+    }
+
+    @GetMapping("/private/check")
+    public String privateCheck() {
+        return "Success Private First API";
+    }
+}
